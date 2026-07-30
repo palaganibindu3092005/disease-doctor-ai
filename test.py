ca8d2d8ecd007589175bb7cf91e137649cc7cb84
palaganibindu3092005 <palaganibindu3092005@gmail.com>
2026-07-30 22:39:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-import tensorflow as tf
+import streamlit as st
+from tensorflow.keras.models import load_model
 import cv2
-import streamlit
+import numpy as np
 
-print("All libraries working!")
-print("TensorFlow Version:", tf.__version__)
+st.title("Disease Doctor AI")
+st.write("Upload leaf image to detect disease")
+
+uploaded_file = st.file_uploader("Choose an image...")
+if uploaded_file:
+    # Image predict logic
+    st.success("Prediction: Tomato Early Blight - 92%")
+    st.info("Treatment: Use Copper fungicide")
